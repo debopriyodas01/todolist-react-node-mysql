d71a60b6920eb412b2b8ed14d2a3fe6cf32f86ef
debopriyodas01 <debopriyodas01@gmail.com>
2026-07-27 17:02:02 +0530

    Update todos.js

--- server/routers/todos.js
+++ server/routers/todos.js
@@ -1,27 +1,38 @@
-const {Router} = require('express');
-const {TodoRecord} = require('../records/todo.record');
-const {pool} = require('../utils/db');
+const { Router } = require('express');
+const { TodoRecord } = require('../records/todo.record');
 
 const TodoRouter = Router();
 
-TodoRouter.get('/', async (req, res) => {
-  const todosList = await TodoRecord.listAll();
+TodoRouter
+  .get('/', async (req, res) => {
+    const todosList = await TodoRecord.listAll();
+    res.status(200).json(todosList);
+  })
 
-  res.send(todosList);
-})
   .post('/create', async (req, res) => {
     const newTodo = new TodoRecord(req.body);
     await newTodo.insert();
 
-    res.send('Values inserted successfully');
+    res.status(201).json({
+      message: 'Values inserted successfully',
+      todo: newTodo,
+    });
   })
+
   .delete('/:id', async (req, res) => {
     const todo = await TodoRecord.getOne(req.params.id);
     await todo.delete();
+
+    res.sendStatus(204);
   })
+
   .put('/update/:id', async (req, res) => {
     const todo = await TodoRecord.getOne(req.params.id);
     await todo.update(req.body.id, req.body.todo);
+
+    res.status(200).json({
+      message: 'Todo updated successfully',
+    });
   });
 
 module.exports = {
