5b7db822d148ca209a54458f5b34cb5b451f36d7
debopriyodas01 <debopriyodas01@gmail.com>
2026-07-27 12:25:48 +0530

    Update db.js

--- server/utils/db.js
+++ server/utils/db.js
@@ -1,11 +1,11 @@
 const { createPool } = require("mysql2/promise");
 
 const pool = createPool({
-  host: process.env.DB_HOST || "mysql",
-  port: process.env.DB_PORT || 3306,
-  user: process.env.DB_USER || "todo",
-  password: process.env.DB_PASSWORD || "todo123",
-  database: process.env.DB_NAME || "todolist",
+  host: process.env.DB_HOST,
+  port: process.env.DB_PORT,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
   namedPlaceholders: true,
   waitForConnections: true,
   connectionLimit: 10,
