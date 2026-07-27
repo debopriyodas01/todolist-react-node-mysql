8bfcb1073742c452c8574107f28f02061bf84db0
debopriyodas01 <debopriyodas01@gmail.com>
2026-07-27 12:53:17 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -1,17 +1,30 @@
-const express = require('express');
-const cors = require('cors');
-const {TodoRouter} = require('./routers/todos');
-require('./utils/db');
+const express = require("express");
+const cors = require("cors");
+
+const { TodoRouter } = require("./routers/todos");
+const { migrate } = require("./utils/db");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
-app.use('/', TodoRouter);
+app.use("/", TodoRouter);
 
 const PORT = process.env.PORT || 8080;
 
-app.listen(PORT, () => {
-  console.log(`Listening on port ${PORT}`);
-});
+async function start() {
+  try {
+    await migrate();
+
+    app.listen(PORT, () => {
+      console.log(`Server listening on ${PORT}`);
+    });
+  } catch (err) {
+    console.error("Failed to initialize database");
+    console.error(err);
+    process.exit(1);
+  }
+}
+
+start();
